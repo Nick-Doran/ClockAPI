0c642f1ac6a816311019978f24929a4a3ed26053
Nick Doran <chasedoran6@gmail.com>
2019-01-30 10:46:54 -0500

    tweaked UI, fixed layout to link to /home/seeallQueries

--- Clockwork.Web/Controllers/HomeController.cs
+++ Clockwork.Web/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace Clockwork.Web.Controllers
 
         public ActionResult SeeAllQueries()
         {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewBag.Title = "Clockwork Web Project";
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            
+
             return View();
         }
 
